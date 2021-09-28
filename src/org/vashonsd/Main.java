cb06ee10ef943f560acfc71ee7d293b95c48b20e
mazur.spencer <mazur.spencer@vashonsd.org>
2021-09-28 11:02:28 -0700

    Second commit

---
+++ src/org/vashonsd/Main.java
@@ -0,0 +1,9 @@
+package org.vashonsd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Comment added to force commit
+	    System.out.println("Hello, world!");
+    }
+}
